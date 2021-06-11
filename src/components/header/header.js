98b0866417558214062531b21971c1c10567f34f
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2021-06-11 11:27:29 +0200

    Add Seo components

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import layoutStyles from '../../styles/layout.module.css';
+
+function Header() {
+  return (
+    <div className={layoutStyles.containerFluid}>
+      <p>header</p>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/layout.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Sticky from 'react-stickynode';
+import Header from './header/header';
+import Footer from './footer/footer';
+
+import layoutStyles from '../styles/layout.module.css';
+
+function Layout({ children }) {
+  const [isSticky, setIsSticky] = useState(false);
+
+  const handleStateChange = (status) => {
+    if (status.status === Sticky.STATUS_FIXED) {
+      setIsSticky(true);
+    } else if (status.status === Sticky.STATUS_ORIGINAL) {
+      setIsSticky(false);
+    }
+  };
+  return (
+    <>
+      <Sticky innerZ={1001} top={0} onStateChange={handleStateChange}>
+        <Header className={`${isSticky ? 'sticky' : 'unSticky'}`}>menu</Header>
+      </Sticky>
+      <main className={layoutStyles.container}>
+        <p>layout</p>
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
